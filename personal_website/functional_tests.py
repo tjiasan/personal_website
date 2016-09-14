9e96a4fe43bfafcdce47deff90a03cc207b90654
Jason <jason.tjia@gmail.com>
2016-09-14 16:05:55 -0400

    first functional tests working

--- personal_website/functional_tests.py
+++ personal_website/functional_tests.py
@@ -1,11 +1,28 @@
 import os
 from selenium import webdriver
 from pyvirtualdisplay import Display
+import unittest
 
 display = Display(visible=0, size=(800, 600))
 display.start()
-driver = webdriver.Chrome()
-driver.get("http://www.google.com")
-print driver.title
-driver.quit()
-display.stop()
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.display= Display(visible=0, size=(800, 600))
+        self.display.start()
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+        self.display.stop()
+    def test_can_open_up_webpage_just_fine(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('Jason', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
