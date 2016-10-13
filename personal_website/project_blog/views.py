3598ef9bad5be0b2dbec8ed6c5cf42af3fdf0c64
Jason <jason.tjia@gmail.com>
2016-10-13 00:15:49 +0000

    finished overview sections

--- personal_website/project_blog/views.py
+++ personal_website/project_blog/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
+from .models import WebContent, BioContent, MachineContent
 
 def web_dev_main(request):
     template = "project_blog/web_dev_overview.html"
-    return render (request, template)
+    content= WebContent.objects.get(title= "overview")
+    
+    return render (request, template,{'content':content})
 
 def biology_main(request):
     template = "project_blog/biology_overview.html"
-    return render (request, template) 
+    content= BioContent.objects.get(title= "overview")
+    return render (request, template, {'content':content}) 
 
 def machine_main(request):
     template = "project_blog/machine_overview.html"
-    return render (request, template) 
+    content= MachineContent.objects.get(title= "overview")
+    return render (request, template,{'content':content}) 
+
+def web_dev(request):
+    pass
+
+def biology(request):
+    pass
+
+def machine(request):
+    pass
+
 
 
 # Create your views here.
